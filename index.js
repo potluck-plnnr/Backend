cbb24167a63e1e207c1452ba5fe8585cff168897
Andrew Haberman <a.haberman25@gmail.com>
2020-07-21 10:54:40 -0400

    user login/reg/get /knex

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-// it's recommended to load configuration for .env as early as possible
-require('dotenv').config(); // add this line as the first thing to run1
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const session = require("express-session")
+const KnexSessionStore = require("connect-session-knex")(session)
+const db = require("./data/config")
+const usersRouter = require("./users/router.js")
 
-const server = require('./api/server');
+const server = express()
+const port = process.env.PORT || 5000
 
-// we'll read the port from the server environment if it is there
-// heroku will have the PORT environment variable set
-const port = process.env.PORT || 5000;
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+server.use(session({
+	resave: false, // avoids recreating sessions that have not changed
+	saveUninitialized: false, // comply with GDPR laws
+	secret: "rawrrrr",
+	store: new KnexSessionStore({
+		knex: db, // configured instance of Knex, or the live database connection
+		createtable: true, // if the session table does not exist, create it
+	}),
+}))
 
-// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+server.use(usersRouter)
+
+server.get("/", (req, res, next) => {
+    res.send("CONNECTED!")
+}) 
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
-});
+	console.log(`Running at http://localhost:${port}`)
+})
