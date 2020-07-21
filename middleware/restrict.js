2442a764eceed16bd48a8080f4c9384388046415
Andrew Haberman <a.haberman25@gmail.com>
2020-07-21 11:56:58 -0400

    updated for jwt, has session code still

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,17 +1,66 @@
-function restrict() {
-	const authError = {
-		message: "Invalid credentials",
-	}
+// function restrict() {
+// 	const authError = {
+// 		message: "Invalid credentials",
+// 	}
 	
+// 	return async (req, res, next) => {
+// 		try {
+// 			if (!req.session || !req.session.user) {
+// 				return res.status(401).json(authError)
+// 			}
+
+// 			// if we reach this point, the user is considered authorized!
+// 			next()
+// 		} catch (err) {
+// 			next(err)
+// 		}
+// 	}
+// }
+
+// module.exports = restrict
+
+const jwt = require("jsonwebtoken")
+
+// const roles = [
+//     "suspended",
+// 	"sales",
+// 	"management",
+// ]
+
+function restrict(role) {
 	return async (req, res, next) => {
+		const authError = {
+			message: "Invalid credentials",
+		}
+		// const authError2 = {
+		// 	message: "Invalid credentials 2",
+		// }
+		const authError3 = {
+			message: "Invalid credentials 3",
+		}
+
 		try {
-			if (!req.session || !req.session.user) {
-				return res.status(401).json(authError)
+			//const token = req.cookies.token // read cookie
+			const token = req.headers.token // for build week
+
+			if (!token) {
+				return res.status(401).json(authError3)
 			}
 
-			// if we reach this point, the user is considered authorized!
-			next()
-		} catch (err) {
+			jwt.verify(token, "some ssecret", (err, decoded) => { // jwt.verify - the variable with token, secret, err
+				if (err) {
+					return res.status(401).json(authError)
+				}
+
+				// if (role && roles.indexOf(decoded.department) < roles.indexOf(role)) {
+				// 	return res.status(401).json(authError2)
+				// }
+
+				next()
+			})	
+			
+			
+		} catch(err) {
 			next(err)
 		}
 	}
--- users/router.js
+++ users/router.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const Users = require("./model")
 const restrict = require("../middleware/restrict")
 
@@ -15,7 +16,7 @@ router.get("/users", restrict(), async (req, res, next) => {
 
 router.post("/register", async (req, res, next) => {
 	try {
-		const { username, password } = req.body
+		const { username, password, department } = req.body
 		const user = await Users.findBy({ username }).first()
 
 		if (user) {
@@ -28,6 +29,7 @@ router.post("/register", async (req, res, next) => {
 			username,
 			// hash the password with a time complexity of "14"
 			password: await bcrypt.hash(password, 14),
+			department
 		})
 
 		res.status(201).json(newUser)
@@ -58,9 +60,17 @@ router.post("/login", async (req, res, next) => {
 
 		// generate a new session for this user,
 		// and sends back a session ID
-		req.session.user = user
+		// req.session.user = user
 
+		const payload = {
+			userId: user.id,
+			username: user.username,
+			//department: user.department, // would normally come from database
+		}
+
+		//res.cookie("token", jwt.sign(payload, process.env.JWT_SECRET || "some ssecret")) // sets cookie DONT USE IN BUILD WEEK
 		res.json({
+			token: jwt.sign(payload, process.env.JWT_SECRET || "some ssecret"), // for build week do tokens
 			message: `Welcome ${user.username}!`,
 		})
 	} catch(err) {
@@ -69,17 +79,97 @@ router.post("/login", async (req, res, next) => {
 })
 
 router.get("/logout", async (req, res, next) => {
-	try {
-		req.session.destroy((err) => {
-			if (err) {
-				next(err)
-			} else {
-				res.status(204).end()
-			}
-		})
-	} catch (err) {
-		next(err)
-	}
+    res.clearCookie("token");
+    res.send("cookie has been eaten")
 })
 
 module.exports = router
+
+
+
+
+// const express = require("express")
+// const bcrypt = require("bcryptjs")
+// const Users = require("./model")
+// const restrict = require("../middleware/restrict")
+
+// const router = express.Router()
+
+// router.get("/users", restrict(), async (req, res, next) => {
+// 	try {
+// 		res.json(await Users.find())
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+// router.post("/register", async (req, res, next) => {
+// 	try {
+// 		const { username, password } = req.body
+// 		const user = await Users.findBy({ username }).first()
+
+// 		if (user) {
+// 			return res.status(409).json({
+// 				message: "Username is already taken",
+// 			})
+// 		}
+
+// 		const newUser = await Users.add({
+// 			username,
+// 			// hash the password with a time complexity of "14"
+// 			password: await bcrypt.hash(password, 14),
+// 		})
+
+// 		res.status(201).json(newUser)
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+// router.post("/login", async (req, res, next) => {
+// 	try {
+// 		const { username, password } = req.body
+// 		const user = await Users.findBy({ username }).first()
+		
+// 		if (!user) {
+// 			return res.status(401).json({
+// 				message: "Invalid Credentials",
+// 			})
+// 		}
+
+// 		// hash the password again and see if it matches what we have in the database
+// 		const passwordValid = await bcrypt.compare(password, user.password)
+
+// 		if (!passwordValid) {
+// 			return res.status(401).json({
+// 				message: "Invalid Credentials",
+// 			})
+// 		}
+
+// 		// generate a new session for this user,
+// 		// and sends back a session ID
+// 		req.session.user = user
+
+// 		res.json({
+// 			message: `Welcome ${user.username}!`,
+// 		})
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+// router.get("/logout", async (req, res, next) => {
+// 	try {
+// 		req.session.destroy((err) => {
+// 			if (err) {
+// 				next(err)
+// 			} else {
+// 				res.status(204).end()
+// 			}
+// 		})
+// 	} catch (err) {
+// 		next(err)
+// 	}
+// })
+
+// module.exports = router
